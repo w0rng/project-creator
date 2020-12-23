3bb0302c52c49e4c27f2c108eaa56ac0f5af3677
w0rng <antonabramov2000@gmail.com>
2020-12-23 12:44:05 +0700

    Новый ридми

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import projectcreator
+from os.path import join, dirname
 
 
 setup(
@@ -7,10 +8,11 @@ setup(
     version=projectcreator.__version__,
     author=projectcreator.__author__,
     description='Package for create projects',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
     url='https://github.com/w0rng/project-creator',
     packages=find_packages(),
     entry_points={
         'console_scripts':
             ['create_project = projectcreator:main']
         }
-) 
+)
